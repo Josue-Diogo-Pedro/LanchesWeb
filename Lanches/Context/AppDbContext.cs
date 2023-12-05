168fbde0b419e115499c1e5622383fe739967452
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-05 12:57:09 +0100

    Adding AppDbContext class and register in services

---
+++ Lanches/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Lanches.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Lanches.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Lanche> Lanches { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=172.31.6.9; Database=HeartEFCore;User ID=sa;Password=AT@123;pooling=true;TrustServerCertificate=true";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
--- Lanches/Program.cs
+++ Lanches/Program.cs
@@ -1,4 +1,12 @@
+using Lanches.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// - Registrando serviço do DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
