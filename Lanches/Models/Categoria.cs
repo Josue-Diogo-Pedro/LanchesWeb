9bd3eb0ca7db1663938485d876cb47f6398e380b
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-05 12:38:33 +0100

    - Add Categoria and Lanche classes
    - Add packages: EntityFrameworkCore | SqlServer | Design | Tools

---
+++ Lanches/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace Lanches.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+
+    public List<Lanche> Lanches { get; set; }
+}
