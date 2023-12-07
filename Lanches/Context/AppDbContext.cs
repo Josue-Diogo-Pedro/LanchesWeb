4da626a42bca6a000dd0f0e496fb79987556c62c
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-07 14:36:00 -0800

    Adding a classe CarrinoCompraItem and generate migration for them

--- Lanches/Context/AppDbContext.cs
+++ Lanches/Context/AppDbContext.cs
@@ -6,6 +6,7 @@ public class AppDbContext : DbContext
 {
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Lanche> Lanches { get; set; }
+    public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
---
+++ Lanches/Models/CarrinhoCompraItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lanches.Models;
+
+public class CarrinhoCompraItem
+{
+    public int CarrinhoCompraItemId { get; set; }
+    public Lanche Lanche { get; set; }
+
+    public int Quantidade { get; set; }
+
+    [StringLength(200)]
+    public string CarrinhoCompraId { get; set; }
+}
