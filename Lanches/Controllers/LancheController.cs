8b9e744d03c51df6610b7ae00b221b2424d080a3
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-07 14:15:21 -0800

    Adding partial view to show resume of Lanches

---
+++ Lanches/Controllers/LancheController.cs
@@ -0,0 +1,27 @@
+﻿using Lanches.Repositories.Interfaces;
+using Lanches.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Controllers;
+
+public class LancheController : Controller
+{
+    private readonly ILancheRepository _lancheRepository;
+
+    public LancheController(ILancheRepository lancheRepository)
+    {
+        _lancheRepository = lancheRepository;
+    }
+
+    public IActionResult List()
+    {
+        //var lanches = _lancheRepository.Lanches;
+        //return View(lanches);
+
+        var lancheListViewModel = new LancheListViewModel();
+        lancheListViewModel.Lanches = _lancheRepository.Lanches;
+        lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+        return View(lancheListViewModel);
+    }
+}
---
+++ Lanches/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Lanches.Models;
+
+namespace Lanches.ViewModels;
+
+public class LancheListViewModel
+{
+    public IEnumerable<Lanche> Lanches { get; set; }
+    public string CategoriaAtual { get; set; }
+}
