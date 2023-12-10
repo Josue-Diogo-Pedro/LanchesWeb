44ff23aa84b8e51ffb95bfde7484ce48fe25d622
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-10 12:54:16 -0800

    Creating ViewModel CategoriaMenu to show all categories in dropdown and open the especify action with especify category in controller LancheController

---
+++ Lanches/Components/CategoriaMenu.cs
@@ -0,0 +1,20 @@
+﻿using Lanches.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Components;
+
+public class CategoriaMenu : ViewComponent
+{
+    private readonly ICategoriaRepository _categoriaRepository;
+
+	public CategoriaMenu(ICategoriaRepository categoriaRepository)
+	{
+		_categoriaRepository = categoriaRepository;
+	}
+
+	public IViewComponentResult Invoke()
+	{
+		var categorias = _categoriaRepository.Categorias.OrderBy(l => l.CategoriaNome);
+		return View(categorias);
+	}
+}
