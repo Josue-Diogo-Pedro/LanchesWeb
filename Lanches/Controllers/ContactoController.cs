97480c48d2405e1d26c6ec9fbe2bcf15779696d4
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-09 23:12:07 -0800

    Creating TagHelper to Controller Contacto

---
+++ Lanches/Controllers/ContactoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Controllers;
+
+public class ContactoController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
