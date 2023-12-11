77234be59d67a99b84038ee148644a032a00343f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-11 10:43:08 -0800

    Creating PedidoController

---
+++ Lanches/Controllers/PedidoController.cs
@@ -0,0 +1,29 @@
+﻿using Lanches.Models;
+using Lanches.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Controllers;
+
+public class PedidoController : Controller
+{
+    private readonly IPedidoRepostitory _pedidoRepository;
+    private readonly CarrinhoCompra _carrinhoCompra;
+
+    public PedidoController(IPedidoRepostitory pedidoRepository, CarrinhoCompra carrinhoCompra)
+    {
+        _pedidoRepository = pedidoRepository;
+        _carrinhoCompra = carrinhoCompra;
+    }
+
+    [HttpGet]
+    public IActionResult Checkout()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Checkout(Pedido pedido)
+    {
+        return View();
+    }
+}
