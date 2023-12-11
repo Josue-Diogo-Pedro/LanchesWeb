7df1dd194a15993ac9a3b821fea5e2269568afa1
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-11 13:03:01 -0800

    Creating method Checkout(post) to create a Pedido

--- Lanches/Controllers/PedidoController.cs
+++ Lanches/Controllers/PedidoController.cs
@@ -24,6 +24,46 @@ public class PedidoController : Controller
     [HttpPost]
     public IActionResult Checkout(Pedido pedido)
     {
-        return View();
+        int totalItensPedido = 0;
+        decimal precoTotalPedido = 0.0m;
+
+        //Obtem os itens do carrinho de compra do cliente
+        List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
+        _carrinhoCompra.CarrinhoCompraItens = itens;
+
+        //Verifica se existem itens de pedido
+        if(_carrinhoCompra.CarrinhoCompraItens.Count == 0)
+        {
+            ModelState.AddModelError("", "O seu carrinho está vazio, que tal incluir um lanche?");
+        }
+
+        foreach (var item in itens)
+        {
+            totalItensPedido += item.Quantidade;
+            precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+        }
+
+        //Atribui os valores ao pedido
+        pedido.TotalItensPedido = totalItensPedido;
+        pedido.PedidoTotal = precoTotalPedido;
+
+        //Valida os dados do pedido
+        if (ModelState.IsValid)
+        {
+            //Cria o pedido e os detalhes
+            _pedidoRepository.CriarPedido(pedido);
+
+            //Define mensagens do cliente
+            ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+            ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+            //Limpa o carrinho do cliente
+            _carrinhoCompra.LimparCarrinho();
+
+            //Exibe a view com dos dados do pedido e do cliente
+            return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+        }
+
+        return View(pedido);
     }
 }
