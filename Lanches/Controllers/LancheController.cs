904ae791bc599d958a5aac9a3da2c500bf4d9466
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-11 15:56:00 +0100

    Search lanche for name

--- Lanches/Controllers/LancheController.cs
+++ Lanches/Controllers/LancheController.cs
@@ -60,4 +60,35 @@ public class LancheController : Controller
 
         return View(lanche);
     }
+
+    public ViewResult Search(string searchString)
+    {
+        IEnumerable<Lanche> lanches;
+        string categoriaAtual = string.Empty;
+
+        if (string.IsNullOrEmpty(searchString))
+        {
+            lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+            categoriaAtual = "Todos os Lanches";
+        }
+        else
+        {
+            lanches = _lancheRepository.Lanches
+                .Where(p => p.Nome.ToLower().Contains(searchString.ToLower()));
+
+            if(lanches.Any()) {
+                categoriaAtual = "Lanches";
+            }
+            else
+            {
+                categoriaAtual = "Nenhum lanche foi encontrado";
+            }
+        }
+
+        return View("~/Views/Lanche/List.cshtml", new LancheListViewModel
+        {
+            Lanches = lanches,
+            CategoriaAtual = categoriaAtual
+        });
+    }
 }
