1f85625c6919b3d9beeb478bd797eef9849d58b3
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-14 11:12:43 +0100

    Add _Layout, _ViewImports and _ViewStarts

--- Lanches/Program.cs
+++ Lanches/Program.cs
@@ -68,7 +68,7 @@ app.UseEndpoints(endpoints =>
 {
 	endpoints.MapControllerRoute(
 	  name: "areas",
-	  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+	  pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
 	);
 });
 
