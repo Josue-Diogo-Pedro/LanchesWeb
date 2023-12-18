65acad8a06a83abb262b307f6ee235f2125a1fe8
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-17 19:12:41 -0800

    Fishing to edit controller for RelatorioVendas

--- Lanches/Areas/Admin/Controllers/AdminRelatorioVendasController.cs
+++ Lanches/Areas/Admin/Controllers/AdminRelatorioVendasController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lanches.Areas.Admin.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Schema;
 
-namespace Lanches.Areas.Admin.Controllers
+namespace Lanches.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class AdminRelatorioVendasController : Controller
 {
-    [Area("Admin")]
-    public class AdminRelatorioVendasController : Controller
+    private readonly RelatorioVendasServices _relatorioVendasServices;
+
+    public AdminRelatorioVendasController(RelatorioVendasServices relatorioVendasServices)
     {
-        public IActionResult Index()
+        _relatorioVendasServices = relatorioVendasServices;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> RelatorioVendasSimples(DateTime? minDate, DateTime? maxDate)
+    {
+        if (!minDate.HasValue)
         {
-            return View();
+            minDate = new DateTime(DateTime.Now.Year, 1, 1);
         }
+        if (!maxDate.HasValue)
+        {
+            maxDate = DateTime.Now;
+        }
+
+        ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+        ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+        var result = await _relatorioVendasServices.FindByDateAsync(minDate, maxDate);
+
+        return View(result);
     }
 }
