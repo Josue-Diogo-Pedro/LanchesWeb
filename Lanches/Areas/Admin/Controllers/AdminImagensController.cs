0040ba864862cfb4f1724dfebb69221472c314ac
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-19 09:10:53 +0100

    Add AdminImagensController

---
+++ Lanches/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,22 @@
+﻿using Lanches.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Lanches.Areas.Admin.Controllers;
+
+public class AdminImagensController : Controller
+{
+    private readonly ConfigurationImagens _conf;
+    private readonly IWebHostEnvironment _hostingEnvironment;
+
+    public AdminImagensController(IWebHostEnvironment hostingEnvironment, IOptions<ConfigurationImagens> conf)
+    {
+        _conf = conf.Value;
+        _hostingEnvironment = hostingEnvironment;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
