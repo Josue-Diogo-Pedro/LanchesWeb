b4d3e077d6da4b4b73199a80561119771a15db0d
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-20 12:29:37 +0100

    Creating a class LancheGrafico to manage google charts

---
+++ Lanches/Models/LancheGrafico.cs
@@ -0,0 +1,8 @@
+﻿namespace Lanches.Models;
+
+public class LancheGrafico
+{
+	public string LancheNome { get; set; }
+	public int LancheQuantidade { get; set; }
+	public decimal LancheValorTotal { get; set; }
+}
