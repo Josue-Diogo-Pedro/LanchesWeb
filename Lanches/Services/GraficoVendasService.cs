9f27c239fa5dd4b7a2e7cfe0eee11b79e6f4b7af
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-21 10:05:31 +0100

    Create a method to generate a GraficoVendas

--- Lanches/Services/GraficoVendasService.cs
+++ Lanches/Services/GraficoVendasService.cs
@@ -1,6 +1,45 @@
-﻿namespace Lanches.Services;
+﻿using Lanches.Context;
+using Lanches.Models;
+
+namespace Lanches.Services;
 
 public class GraficoVendasService
 {
+    public readonly AppDbContext _context;
 
+    public GraficoVendasService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<LancheGrafico> GetVendasLanches(int dias = 360)
+    {
+        var data = DateTime.Now.AddDays(-dias);
+
+        var lanches = (from pd in _context.PedidoDetalhes
+                       join l in _context.Lanches on pd.LancheId equals l.LancheId
+                       where pd.Pedido.PedidoEnviado >= data
+                       group pd by new { pd.PedidoId, l.Nome, pd.Quantidade }
+                       into g
+                       select new
+                       {
+                           LancheNome = g.Key.Nome,
+                           LanchesQuantidade = g.Sum(q => q.Quantidade),
+                           LanchesValorTotal = g.Sum(vt => vt.Quantidade * vt.Lanche.Preco)
+                       });
+
+        List<LancheGrafico> graficosLanche = new();
+
+        foreach (var item in lanches)
+        {
+            var lanche = new LancheGrafico();
+            lanche.LancheNome = item.LancheNome;
+            lanche.LancheQuantidade = item.LanchesQuantidade;
+            lanche.LancheValorTotal = item.LanchesValorTotal;
+
+            graficosLanche.Add(lanche);
+        }
+
+        return graficosLanche;
+    }
 }
