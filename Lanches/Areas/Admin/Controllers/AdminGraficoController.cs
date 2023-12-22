f0c1adcef9a30181c49836f501c2ac577599553e
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-21 19:56:08 -0800

    Controller AdminGrafico with initial configurations

--- Lanches/Areas/Admin/Controllers/AdminGraficoController.cs
+++ Lanches/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lanches.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Lanches.Areas.Admin.Controllers;
 
 public class AdminGraficoController : Controller
 {
+    private readonly GraficoVendasService _graficoVendas;
+
+    public AdminGraficoController(GraficoVendasService graficoVendas)
+    {
+        _graficoVendas = graficoVendas ?? throw new ArgumentNullException(nameof(GraficoVendasService));
+    }
+
+    public JsonResult VendasLanches(int dias)
+    {
+        var lanchesVendasTotais = _graficoVendas.GetVendasLanches(dias);
+        return Json(lanchesVendasTotais);
+    }
+
+
     public IActionResult Index()
     {
         return View();
     }
+
+    [HttpGet]
+    public IActionResult VendasMensais(int dias)
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult VendasSemanais(int dias)
+    {
+        return View();
+    }
+
 }
