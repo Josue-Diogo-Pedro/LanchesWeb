c90cc70380696babdf12d291835e330716935e52
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-21 19:49:55 -0800

    Add AdminGraficoController

---
+++ Lanches/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Areas.Admin.Controllers;
+
+public class AdminGraficoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
