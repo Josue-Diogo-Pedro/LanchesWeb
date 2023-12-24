6476e13e9d8866c7bc97ca819fb5f38a1c591a7f
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-24 05:13:40 -0800

    Starting to define

--- Lanches/Areas/Admin/Views/FastReportUtils/HelperFastReport.cs
+++ Lanches/Areas/Admin/Views/FastReportUtils/HelperFastReport.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Reflection;
 
 namespace Lanches.Areas.Admin.Views.FastReportUtils;
 
@@ -7,5 +8,13 @@ public class HelperFastReport
     public static DataTable GetDataTable<TEntity>(IEnumerable<TEntity> table, string name) where TEntity : class
     {
         var offset = 78;
+        DataTable result = new(name);
+
+        PropertyInfo[] infos = typeof(TEntity).GetProperties();
+        foreach (PropertyInfo info in infos)
+        {
+            if(info.PropertyType.IsGenericType &&
+                )
+        }
     }
 }
