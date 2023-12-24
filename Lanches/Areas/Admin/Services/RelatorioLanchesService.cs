c41079386cacc0d6ffa0dc7574dd049d761f0f80
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-24 04:46:25 -0800

    Finishing to implement RelatorioLanchesService

--- Lanches/Areas/Admin/Services/RelatorioLanchesService.cs
+++ Lanches/Areas/Admin/Services/RelatorioLanchesService.cs
@@ -1,6 +1,35 @@
-﻿namespace Lanches.Areas.Admin.Services;
+﻿using Lanches.Context;
+using Lanches.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches.Areas.Admin.Services;
 
 public class RelatorioLanchesService
 {
+    private readonly AppDbContext _context;
 
+	public RelatorioLanchesService(AppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IEnumerable<Lanche>> GetLanchesReportAsync()
+	{
+		var lanches = await _context.Lanches.ToListAsync();
+
+		if (lanches is null)
+			return default(IEnumerable<Lanche>);
+
+		return lanches;
+	}
+
+	public async Task<IEnumerable<Categoria>> GetCategoriasAsync()
+	{
+		var categorias = await _context.Categorias.ToListAsync();
+
+		if (categorias is null)
+			return default(IEnumerable<Categoria>);
+
+		return categorias;
+	}
 }
