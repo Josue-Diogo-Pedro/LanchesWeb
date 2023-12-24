75512135f7768e5fd3bfe347dfac419cdd0544bc
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-24 04:56:39 -0800

    Creating a method to convert IEnumerable<T> to DataTabe

--- Lanches/Areas/Admin/Views/FastReportUtils/HelperFastReport.cs
+++ Lanches/Areas/Admin/Views/FastReportUtils/HelperFastReport.cs
@@ -1,6 +1,11 @@
-﻿namespace Lanches.Areas.Admin.Views.FastReportUtils;
+﻿using System.Data;
+
+namespace Lanches.Areas.Admin.Views.FastReportUtils;
 
 public class HelperFastReport
 {
-
+    public static DataTable GetDataTable<TEntity>(IEnumerable<TEntity> table, string name) where TEntity : class
+    {
+        var offset = 78;
+    }
 }
