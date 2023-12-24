06e1b78e4a2225e887fb26971c4e66e19b7b62e1
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-12-24 05:33:26 -0800

    Finishing to define method to convert IEnumerable to DataTable

--- Lanches/Areas/Admin/Views/FastReportUtils/HelperFastReport.cs
+++ Lanches/Areas/Admin/Views/FastReportUtils/HelperFastReport.cs
@@ -14,7 +14,47 @@ public class HelperFastReport
         foreach (PropertyInfo info in infos)
         {
             if(info.PropertyType.IsGenericType &&
-                )
+                info.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                result.Columns.Add(new DataColumn(info.Name, Nullable.GetUnderlyingType(info.PropertyType)));
+            }
+            else
+            {
+                result.Columns.Add(new DataColumn(info.Name, info.PropertyType));  
+            }
         }
+        foreach (var el in table)
+        {
+            DataRow row = result.NewRow();
+            foreach (PropertyInfo info in infos)
+            {
+                if (info.PropertyType.IsGenericType &&
+                    info.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    object t = info.GetValue(el);
+                    if(t is null)
+                    {
+                        t = Activator.CreateInstance(Nullable.GetUnderlyingType(info.PropertyType));
+                    }
+                    row[info.Name] = t;
+                }
+                else
+                {
+                    if(info.PropertyType == typeof(byte[]))
+                    {
+                        var imageData = (byte[])info.GetValue(el);
+                        var bytes = new byte[imageData.Length - offset];
+                        Array.Copy(imageData, offset, bytes, 0, imageData.Length);
+                        row[info.Name] = bytes;
+                    }
+                    else
+                    {
+                        row[info.Name] = info.GetValue(el);
+                    }
+                }
+            }
+            result.Rows.Add(row);
+        }
+        return result;
     }
 }
